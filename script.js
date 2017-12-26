2a6044c037117ef7ce135d417960243c71e992bf
tintinve <pedr0261@stud.kea.dk>
2017-12-26 23:33:01 +0100

    V0.05 carousel spins with click and arrow key press

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
 let hexagono = document.querySelector(".hexagono");
 let boton_next = document.querySelector(".next");
 let boton_prev = document.querySelector(".prev");
-let img_frontal  = 0;
+let img_frontal = 0;
+window.addEventListener("keydown", teclado);
 boton_next.addEventListener('click', funcion_next);
 boton_prev.addEventListener('click', funcion_prev);
 
+
+function teclado(e) {
+    if (e.keyCode == "37") {
+        img_frontal = img_frontal - 60;
+        hexagono.style.transform = "rotateY(" + img_frontal + "deg)";
+    }
+    if (e.keyCode == "39") {
+        img_frontal = img_frontal + 60;
+        hexagono.style.transform = "rotateY(" + img_frontal + "deg)";
+    }
+
+}
+
 function funcion_next(){
    if(img_frontal == 0){
-       img_frontal = img_frontal - 60;
+       img_frontal = img_frontal + 60;
        hexagono.style.transform = "rotateY("+img_frontal+"deg)";
    }  else {
-       img_frontal = img_frontal - 60;
+       img_frontal = img_frontal + 60;
        hexagono.style.transform = "rotateY("+img_frontal+"deg)";
    }
 }
 function funcion_prev(){
    if(img_frontal == 0){
-       img_frontal = img_frontal + 60;
+       img_frontal = img_frontal - 60;
        hexagono.style.transform = "rotateY("+img_frontal+"deg)";
    }  else {
-       img_frontal = img_frontal + 60;
+       img_frontal = img_frontal - 60;
        hexagono.style.transform = "rotateY("+img_frontal+"deg)";
    }
 }
